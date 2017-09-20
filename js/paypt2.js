0518c28b4ce2ea44ac13f5f97d35324ac4c968d6
Gisselle Ortega-Morales <gmorales444@gmail.com>
2017-09-20 14:59:22 -0700

    hkjl

--- js/paypt2.js
+++ js/paypt2.js
@@ -11,19 +11,31 @@
 
 "use strict";
 
+// GOM ~ makes the select boxes initially blank
+function removeSelectDefaults() {
+  var emptySelect = document.getElementsByTagName("select"); // GOM ~ gets the select elements from the DOM
 
+  for (var i = 0; i < emptySelect.length; i++) {
+    emptySelect[i].selectedIndex = -1; // GOM ~ tells DOM to turn off selected Index (the default)
+  }
+}
 
+function PersonalInformation() {
 
+}
 
+// creates event listeners for functions you dont necissarily want to work as soon as the page loads
 function createEventListeners() {
 
 }
 
+// GOM ~ functions will carry out as soon as the page loads
 function setUpPage() {
   createEventListeners();
+  removeSelectDefaults();
 }
 
-// GOM ~ Loads the functions in set up page as soon as the page loads 
+// GOM ~ Loads the functions in set up page as soon as the page loads
 if (window.addEventListener) {
   window.addEventListener("load", setUpPage, false);
 }
