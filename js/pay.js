c6e5d11144af7e32feb01fcb3ee33bf983290a9e
Marco Tapia <mtapia450@west-mec.org>
2017-09-20 13:18:51 -0700

    Started Off Template for pay.js

---
+++ js/pay.js
@@ -0,0 +1,10 @@
+/*
+    pay.js
+    Form Validation
+    By: Marco, Gisselle, and Mason
+
+*/
+
+/*=========================================================================//
+Start-- Marco's Section: Payment Information, Bottom half of Payment Page
+//========================================================================*/
