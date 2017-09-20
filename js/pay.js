898b7f79a774496fcdbcadf231ef611c6d898d65
Marco Tapia <mtapia450@west-mec.org>
2017-09-20 14:54:35 -0700

    A

--- js/pay.js
+++ js/pay.js
@@ -9,14 +9,60 @@
 */
 "use strict";
 
+alert("sjdkhdjk");
+
 function setUpPage() {
   createEventListeners();
   removeSelectDefaults();
+  alert("in setUpPage")
+}
+
+function removeSelectDefaults() {
+
+}
+
+function validateForm(submitevt) {
+  if (submitevt.preventDefault) {
+    submitevt.preventDefault();
+  }
+  else {
+    submitevt.returnValue = false;
+  }
+  formValidity = true;
+
+  validateRequired();
+
+
+
+  //validity tests will go here
+  if (formValidity === true) {//form is valid
+     document.getElementById("errorText").innerHTML = "";
+     document.getElementById("errorText").style.display = "none";
+     document.getElementsByTagName("form")[0].submit();
+  }
+  else {//form not valid
+    document.getElementById("errorText").innerHTML = "Please fix the indicated problems."; //message for the error div
+    document.getElementById("errorText").style.display = "block"; //it turns the div visible
+    scroll(0,0) // scrolls you back up to the page so you see the div
+
+  }
+alert("in validateForm");
 }
 
 function createEventListeners() {
+  var submitForm = document.getElementsByTagName("form")[0];
+  if (submitForm.addEventListener) {
+    submitForm.addEventListener("submit", validateForm , false)
+  }
+  else if (submitForm.attachEvent) {
+      submit.attachEvent("onsubmit", validateForm)
 
+          }
+
+
+  alert("in createEventListeners");
 }
+
 
 
 
