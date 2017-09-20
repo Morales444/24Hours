931753cc0e228736e814f76b7dd4d10063cc3699
Marco Tapia <mtapia450@west-mec.org>
2017-09-20 15:24:52 -0700

    A

--- js/pay.js
+++ js/pay.js
@@ -8,63 +8,98 @@
 
 */
 "use strict";
+var formValidity = true;
+
+// GOM ~ makes the select boxes initially blank
+function removeSelectDefaults() {
+  var emptySelect = document.getElementsByTagName("select"); // GOM ~ gets the select elements from the DOM
+
+  for (var i = 0; i < emptySelect.length; i++) {
+    emptySelect[i].selectedIndex = -1; // GOM ~ tells DOM to turn off selected Index (the default)
+  }
+}
+
+function personalInformation() {
+  //GOM ~ variables
+  var inputElements = document.getElementsByTagName("input");
+  var errorDiv = document.getElementById("errorText");
+  var fieldsetValidity = true;
+  var elementCount = inputElements.length;
+  var currentElement;
+
+  // GOM ~ code for the actual validation of the function
+  try {
+    // GOM ~ checks to see if the user put anything in each field
+    for (var i = 0; i < elementCount; i++) {
+      currentElement = inputElements[i];
+      if (currentElement.value === "") {
+        currentElement.style.background = "rgb(255, 100, 100)";
+        fieldsetValidity = false;
+      } else {
+        currentElement.style.background = "white";
+      }
+    }
+
+    if (fieldsetValidity === false) { // GOM ~ Is the error message clients see
+        throw "Please complete the indicated issues";
+    }
+    else { // GOM ~ removes error message
+      errorDiv.style.display = "none";
+      errorDiv.innerHTML = "";
+    }
+
+  } catch (msg) {
+    errorDiv.style.display = "block";
+    errorDiv.innerHTML = msg;
+    errorDiv.style.color = "rgb(255, 100, 100)";
+    formValidity = false;
+  }
+
+}
+
+function validateForm(evt) {
+  // GOM ~ Prevents the form from submiting prematurely
+  if (evt.preventfault) {
+    evt.preventDefault();
+  }
+  else {
+    evt.returnValue = false;
+  }
+  formValidity = true;
+
+  personalInformation();
+
+  if (formValidity === true) { //GOM ~ form is valid
+    document.getElementById("errorText").innerHTML = "";
+    document.getElementById("errorText").style.display = "none";
+    document.getElementsByTagName("form")[0].submit();
+  }
+}
 
 
+// creates event listeners for functions you dont necissarily want to work as soon as the page loads
+function createEventListeners() {
+  var submit = document.getElementsByTagName("form")[0];
+  if (submit.addEventListener) {
+    submit.addEventListener("submit", validateForm, false);
+  } else if (submit.attachEvent) {
+    submit.attachEvent("onsubmit", validateForm);
+  }
+}
+
+// GOM ~ functions will carry out as soon as the page loads
 function setUpPage() {
   createEventListeners();
   removeSelectDefaults();
 }
 
-function removeSelectDefaults() {
-
+// GOM ~ Loads the functions in set up page as soon as the page loads
+if (window.addEventListener) {
+  window.addEventListener("load", setUpPage, false);
 }
-
-function validateForm(submitevt) {
-  if (submitevt.preventDefault) {
-    submitevt.preventDefault();
-  }
-  else {
-    submitevt.returnValue = false;
-  }
-  formValidity = true;
-
-  validateRequired();
-
-
-
-  //validity tests will go here
-  if (formValidity === true) {//form is valid
-     document.getElementById("errorText").innerHTML = "";
-     document.getElementById("errorText").style.display = "none";
-     document.getElementsByTagName("form")[0].submit();
-  }
-  else {//form not valid
-    document.getElementById("errorText").innerHTML = "Please fix the indicated problems."; //message for the error div
-    document.getElementById("errorText").style.display = "block"; //it turns the div visible
-    scroll(0,0) // scrolls you back up to the page so you see the div
-
-  }
-alert("in validateForm");
+else if (window.attachEvent) {
+  window.attachEvent("onload", setUpPage);
 }
-
-function createEventListeners() {
-  var submitForm = document.getElementsByTagName("form")[0];
-  if (submitForm.addEventListener) {
-    submitForm.addEventListener("submit", validateForm , false)
-  }
-  else if (submitForm.attachEvent) {
-      submit.attachEvent("onsubmit", validateForm)
-
-          }
-
-
-  alert("in createEventListeners");
-}
-
-
-
-
-
 
 
 
